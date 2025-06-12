ab865e812212aea074903e4897c7af93274631e9
Kratos <Konibacisse20@gmail.com>
2025-06-12 13:45:31 +0000

    Revert ""Ajout de l'image d'arrière-plan et mise à jour de la mise en page""

--- test.js
+++
@@ -1,4 +0,0 @@
-fetch("http://localhost:5000/data") 
- .then(response => response.json())
-  .then(data => console.log("Données reçues :", data))
-  .catch(error => console.error("Erreur :", error));
