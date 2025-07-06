1abcbf5c3f10ee4cbfba5b3c83bc9b99696e8e78
Boubadev <boubadev@gmail.com>
2025-07-06 21:31:27 +0000

    ej

--- backend/mon-backend-app/src/db.js
+++ backend/mon-backend-app/src/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234567890", // Mets ton mot de passe ici
+  password: "12345567890", // Mets ton mot de passe ici
   database: "gestion_colis", // Mets le nom de ta base ici
 });
 
